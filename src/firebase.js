73be9f71e18a7060fc82e563de58d6e695cdecdb
ismail debbarh <usamdh07@gmail.com>
2023-01-29 19:13:53 +0100

    edit firebase object values

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2ou0nxbqLVoEh3rYOfu1VnsliEZrTMvc",
-  authDomain: "netflix-clone-r-61b35.firebaseapp.com",
-  projectId: "netflix-clone-r-61b35",
-  storageBucket: "netflix-clone-r-61b35.appspot.com",
-  messagingSenderId: "65207706331",
-  appId: "1:65207706331:web:04b113f99a1ed0537f62fe"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
